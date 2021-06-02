24a78117ab5a9d17961f78c618a04a8895969ec4
LivinParadoX <alexandre.janvrin@reseau.eseo.fr>
2021-06-02 10:17:29 +0200

    started using pymetasploit

--- suggestor/cli.py
+++ suggestor/cli.py
@@ -1,30 +1,20 @@
-"""
-"""
-
-from importlib import resources
-
+from pymetasploit3.msfrpc import MsfRpcClient
+from tabulate import tabulate
+from pyfzf.pyfzf import FzfPrompt
 import click
-import icecream
-
 
 @click.command()
-@click.argument("services_csv", type=click.File())
-def main(services_csv):
-    headers = services_csv.readline()
-    services = []
-    for line in services_csv:
-        host,port,proto,name,state,info = line.strip().split(",")
-        if state == '"open"':
-            services.append(
-                    {
-                        "host": host[1:-1],
-                    "port": port[1:-1],
-                    "proto": proto[1:-1],
-                    "name": name[1:-1],
-                    "state": state[1:-1],
-                    "info": info[1:-1]
-
-                        }
-                    )
-    for service in services:
-        print(service)
+@click.option("--rpc-password")
+@click.option("--rpc-port", default=55552)
+@click.option("--rpc-server", default="127.0.0.1")
+@click.option("--rpc-use-ssl", is_flag=True)
+@click.option("--workspace")
+def main(rpc_password, rpc_port, rpc_server, rpc_use_ssl, workspace):
+    if rpc_password is None:
+        rpc_password = click.prompt("Password", hide_input=True)
+    client = MsfRpcClient(rpc_password, server=rpc_server, port=rpc_port, ssl=rpc_use_ssl)
+    if workspace is None:
+        workspace = FzfPrompt().prompt([workspace["name"] for workspace in client.db.workspaces.list])
+    workspace = client.db.workspaces.workspace(workspace)
+    services = workspace.services.find(only_up=True, limit=9223372036854775807)
+    print(tabulate(services, headers="keys"))
